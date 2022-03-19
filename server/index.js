db3fb2daad07ae5468397113c64d1ee43da1733e
fqhd <fahd31256@gmail.com>
2022-03-19 16:40:13 +0000

    average account level per rank

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 dotenv.config();
 const TIERS = ['IRON', 'BRONZE', 'SILVER', 'GOLD', 'PLATINUM', 'DIAMOND', 'MASTER', 'GRANDMASTER', 'CHALLENGER'];
-const TIMEOUT = 100;
+const TIMEOUT = 1500;
 
 function apiCall(url){
     return new Promise((resolve, reject) => {
@@ -25,26 +25,46 @@ async function getRankData(tier){
     return totalGamesPlayed;
 }
 
-async function getPlayerData(){
-    let totalPlayerData = [];
+async function getNumGamesPerRank(){
+    let data = [];
     for(let i = 0; i < TIERS.length; i++){
-        const rankData = await getRankData(TIERS[i]).catch(err => {
-            console.error(err);
-            console.log('index: ' + i);
-            console.log(TIERS.length);
-        });
-        totalPlayerData.push(rankData);
+        const rankData = await getRankData(TIERS[i]);
+        data.push(rankData);
     }
-    return totalPlayerData;
+    return data;
+}
+
+async function getAccountLevelPerRank(){
+    const data = [];
+    for(let i = 0; i < TIERS.length; i++){
+        const response = await apiCall(`https://euw1.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/${TIERS[i]}/I?page=1&api_key=${process.env.RIOT_KEY}`);
+        const json = await response.json();
+        let averageLevel = 0;
+        for(let i = 0; i < json.length; i++){
+            const player = json[i];
+            const accountID = player.summonerId;
+            const accInfoResponse = await apiCall(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/${accountID}?api_key=${process.env.RIOT_KEY}`);
+            const jsonAccountInfo = await accInfoResponse.json();
+            const accountLevel = jsonAccountInfo.summonerLevel;
+            averageLevel += accountLevel;
+        }
+        averageLevel /= json.length;
+        data.push(averageLevel);
+    }
+    return data;
 }
 
 async function main(){
     // Fetch all data
-    const playerData = await getPlayerData();
+    // const numGamesPerRank = await getNumGamesPerRank();
+    const numGamesPerRank = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    // const accountLevelsPerRank = await getAccountLevelPerRank();
+    const accountLevelsPerRank = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     
     // Construct databse json object
     const database = {
-        playerData
+        numGamesPerRank,
+        accountLevelsPerRank
     };
 
     // Save databse to file
