1ee6ac277d20e3720c0757fa6aef50ab78938b7a
fqhd <fahd31256@gmail.com>
2022-03-23 01:18:20 +0000

    update database.json

--- server/index.js
+++ server/index.js
@@ -42,17 +42,18 @@ async function loadPlayers(){
         const response = await apiCall(`https://euw1.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/${TIERS[i]}/I?page=1&api_key=${process.env.RIOT_KEY}`);
         const players = await response.json();
         playerData[TIERS[i].toLowerCase()] = [];
-        for(let j = 0; j < 1; j++){
+        for(let j = 0; j < players.length; j++){
             const accountID = players[j].summonerId;
             const accInfoResponse = await apiCall(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/${accountID}?api_key=${process.env.RIOT_KEY}`);
             const accountInfo = await accInfoResponse.json();
             playerData[TIERS[i].toLowerCase()].push(accountInfo);
         }
     }
+    playerData.numPlayers = playerData[TIERS[0].toLowerCase()].length;
+    console.log('Num players per rank: ' + playerData.numPlayers);
 }
 
 async function getNumGamesPerRank(){
-    if(!playerData[TIERS[0]]) await loadPlayers();
     let data = [];
     for(let i = 0; i < TIERS.length; i++){
         const rankData = await getRankData(TIERS[i]);
@@ -62,11 +63,10 @@ async function getNumGamesPerRank(){
 }
 
 async function getAccountLevelPerRank(){
-    if(!playerData[TIERS[0]]) await loadPlayers();
     const data = [];
     for(let i = 0; i < TIERS.length; i++){
         let averageLevel = 0;
-        for(let j = 0; j < 1; j++){
+        for(let j = 0; j < playerData[TIERS[i].toLowerCase()].length; j++){
             const accountLevel = playerData[TIERS[i].toLowerCase()][j].summonerLevel;
             averageLevel += accountLevel;
         }
@@ -77,9 +77,8 @@ async function getAccountLevelPerRank(){
 }
 
 async function fetchDataForMatches(){
-    if(!playerData[TIERS[0]]) await loadPlayers();
     for(let i = 0; i < TIERS.length; i++){ // Loop through tiers
-        for(let j = 0; j < 1; j++){
+        for(let j = 0; j < playerData[TIERS[i].toLowerCase()].length; j++){
             const player = playerData[TIERS[i].toLowerCase()][j];
             const matchesResponse = await apiCall(`https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${player.puuid}/ids?start=0&count=1&api_key=${process.env.RIOT_KEY}`).catch(err => console.log('failed to fetch data'));
             const matches = await matchesResponse.json();
@@ -107,35 +106,34 @@ async function main(){
     const string = fs.readFileSync('../database.json');
     const database = JSON.parse(string);
 
-    if(!database.numGamesPerRank){
-        database.numGamesPerRank = await getNumGamesPerRank().catch(err => {
-            console.log('failed to fetch number of games per rank');
-            console.log(err);
-        });
-    }
-    if(!database.accountLevelsPerRank){
-        database.accountLevelsPerRank = await getAccountLevelPerRank().catch(err => {
-            console.log('failed to fetch account levels per rank');
-            console.error(err);
-        });
-    }
-
+    await loadPlayers();
     await fetchDataForMatches();
 
-    if(!database.gameModeDistribution){
-        // Create array for each tier
-        database.gameModeDistribution = {};
-        TIERS.forEach(tier => {
-            database.gameModeDistribution[tier.toLowerCase()] = [];
-            const matches = matchesData[tier.toLowerCase()];
-            const obj = getMatchDataFromArray(matches);
-            for(const e in obj){
-                database.gameModeDistribution[tier.toLowerCase()].push(obj[e]);
-            }
-        });
-    }
+    database.numGamesPerRank = await getNumGamesPerRank().catch(err => {
+        console.log('failed to fetch number of games per rank');
+        console.log(err);
+    });
+    database.accountLevelsPerRank = await getAccountLevelPerRank().catch(err => {
+        console.log('failed to fetch account levels per rank');
+        console.error(err);
+    });
+
+    database.gameModeDistribution = calcGameModeDistribution();
 
     fs.writeFileSync('../database.json', JSON.stringify(database));
+}
+
+function calcGameModeDistribution(){
+    const distribution = {};
+    TIERS.forEach(tier => {
+        distribution[tier.toLowerCase()] = [];
+        const matches = matchesData[tier.toLowerCase()];
+        const obj = getMatchDataFromArray(matches);
+        for(const e in obj){
+            distribution[tier.toLowerCase()].push(obj[e]);
+        }
+    });
+    return distribution;
 }
 
 function getMatchDataFromArray(matches){
